d58484af8bca5ad62d81eb467781e719f045e2d3
torihon62 <torihon62@gmail.com>
2017-05-09 04:05:42 +0000

    use pkg

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/torihon62/hello/newmath"
+)
 
 func main() {
-	fmt.Println("Hello, Wolrd")
+	fmt.Println("Hello, World")
+	fmt.Printf("Sqrt(2) = %v\n", newmath.Sqrt(2))
 }
